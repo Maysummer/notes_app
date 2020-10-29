708b944b491c07f61e6b1b827f8a175346b4dfe7
Maysummer <42492293+Maysummer@users.noreply.github.com>
2020-10-29 11:13:16 +0100

    Add files via upload

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -55,9 +55,7 @@ ROOT_URLCONF = 'mynotes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            
-        ],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
