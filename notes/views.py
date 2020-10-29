fa5d5f59947c021f9cd08264d301f2982de0a91b
Maysummer <42492293+Maysummer@users.noreply.github.com>
2020-10-29 11:16:16 +0100

    Add files via upload

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
     return render(request, 'notes/index.html')
+
+class AboutPageView(TemplateView):
+    template_name = 'notes/about.html'
