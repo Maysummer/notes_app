9a128766c426b6dd66786303a379847fd9b70eb1
Nmesoma Udojike <42492293+Maysummer@users.noreply.github.com>
2021-05-19 13:52:58 +0100

    Update urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 from .import views
 from .views import (AboutPageView, NoteDetailView, NoteCreateView, NoteUpdateView, NoteDeleteView,)
