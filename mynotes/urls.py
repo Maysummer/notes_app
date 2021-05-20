d799e1e893fe105eca270e99c14887e915401766
Nmesoma Udojike <42492293+Maysummer@users.noreply.github.com>
2021-05-20 11:25:29 +0100

    Add files via upload

--- mynotes/urls.py
+++ mynotes/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('notes.urls')),
     path('admin/', admin.site.urls),
