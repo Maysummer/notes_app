50fb59a86efcf3e42d1537f4722b94562cac7cfc
Nmesoma Udojike <42492293+Maysummer@users.noreply.github.com>
2021-12-11 21:30:36 +0100

    Add files via upload

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
